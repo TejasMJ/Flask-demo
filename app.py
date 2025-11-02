9064ad84bc5c5206cae15278803c3d1d6bcddf0a
TejasMJ <tejas21files@gmail.com>
2025-11-02 18:43:22 +0530

    Trying the Github Tunelling as well

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Demo project using Flask and deployment on Render and Railway platforms"
+    return "Demo project using Flask and deployment on Render and Railway platforms. And also on - Github Tunelling"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
